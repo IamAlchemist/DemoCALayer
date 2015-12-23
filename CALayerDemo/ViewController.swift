805a63589f865d925ea98c63bd9b2284ce76ba79
IamAlchemist <wizard.lee@qq.com>
2015-12-23 20:27:08 +0800

    minor change

--- CALayerDemo/ViewController.swift
+++ CALayerDemo/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewForLayer: UIView!
+    
+    var firstLayer : CALayer {
+        return viewForLayer.layer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupFirstLayer()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupFirstLayer() {
+        firstLayer.backgroundColor = UIColor.blueColor().CGColor
+        firstLayer.borderWidth = 100
+        firstLayer.borderColor = UIColor.redColor().CGColor
+        firstLayer.shadowOpacity = 0.7
+        firstLayer.shadowRadius = 10
+        
+        firstLayer.contents = UIImage(named: "star")?.CGImage
+        firstLayer.contentsGravity = kCAGravityCenter
     }
-
 
 }
 
