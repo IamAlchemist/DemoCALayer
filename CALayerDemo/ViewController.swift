c2022fffb25ccd43b34f1df097b489b6699cfe06
IamAlchemist <wizard.lee@qq.com>
2015-12-23 20:41:40 +0800

    commit 2

--- CALayerDemo/ViewController.swift
+++ CALayerDemo/ViewController.swift
@@ -33,5 +33,22 @@ class ViewController: UIViewController {
         firstLayer.contentsGravity = kCAGravityCenter
     }
 
+    @IBAction func tapGestureRecognized(sender: UITapGestureRecognizer) {
+        firstLayer.shadowOpacity = firstLayer.shadowOpacity == 0.7 ? 0.0 : 0.7
+    }
+    
+    @IBAction func pinchGestureRecognized(sender: UIPinchGestureRecognizer) {
+        let offset : CGFloat = sender.scale < 1 ? 5.0 : -5.0
+        let oldFrame = firstLayer.frame
+        let oldOrigin = oldFrame.origin
+        let newOrigin = CGPoint(x: oldOrigin.x + offset, y: oldOrigin.y + offset)
+        let newSize = CGSize(width: oldFrame.width + (offset * -2.0), height: oldFrame.height + (offset * -2.0))
+        let newFrame = CGRect(origin: newOrigin, size: newSize)
+        if newFrame.width >= 100.0 && newFrame.width <= 300.0 {
+            firstLayer.borderWidth -= offset
+            firstLayer.cornerRadius += (offset / 2.0)
+            firstLayer.frame = newFrame
+        }
+    }
 }
 
