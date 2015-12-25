256cda765cb7d4b531f3b219ae8ce76ed7738e8a
Wizard Lee <282539259@qq.com>
2015-12-25 11:38:53 +0800

    minor change

--- CALayerDemo/CATextLayerViewController.swift
+++ CALayerDemo/CATextLayerViewController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class CATextLayerViewController : UIViewController {
     
+    @IBOutlet weak var viewForTextLayer: UIView!
+    @IBOutlet weak var fontSizeValueLabel: UILabel!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var wrapTextSwitch: UISwitch!
+    @IBOutlet weak var alignmentModeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var truncateModeSegementedControl: UISegmentedControl!
+    
+    @IBAction func fontChanged(sender: UISegmentedControl) {
+    }
+    
+    @IBAction func fontSizeChanged(sender: UISlider) {
+    }
+    
+    @IBAction func wrapTextSwitchChanged(sender: UISwitch) {
+    }
+    
+    @IBAction func aligmentModeChanged(sender: UISegmentedControl) {
+    }
+    
+    @IBAction func truncatModeChanged(sender: UISegmentedControl) {
+    }
+    
+    
 }
