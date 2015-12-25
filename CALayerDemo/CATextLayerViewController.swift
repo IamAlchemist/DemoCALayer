5a32ccfbc9e7246bdad06cea9490c9b5e229260e
Wizard Lee <282539259@qq.com>
2015-12-25 11:53:02 +0800

    add CATextLayerViewController

--- CALayerDemo/CATextLayerViewController.swift
+++ CALayerDemo/CATextLayerViewController.swift
@@ -17,6 +17,27 @@ class CATextLayerViewController : UIViewController {
     @IBOutlet weak var alignmentModeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var truncateModeSegementedControl: UISegmentedControl!
     
+    enum Font : Int {
+        case Helvetica, NoteworthyLight
+    }
+    
+    enum AlignmentMode : Int {
+        case Left, Center, Justified, Right
+    }
+    
+    enum TruncationMode : Int {
+        case Start, Middle, End
+    }
+    
+    var noteworthyLightFont : AnyObject?
+    var helveticaFont : AnyObject?
+    let baseFontSize : CGFloat = 24.0
+    let textLayer = CATextLayer()
+    var fontSize : CGFloat = 24.0
+    var previouslySelectedTruncationMode = TruncationMode.End
+    
+    // MARK: - IBActions
+    
     @IBAction func fontChanged(sender: UISegmentedControl) {
     }
     
@@ -32,5 +53,49 @@ class CATextLayerViewController : UIViewController {
     @IBAction func truncatModeChanged(sender: UISegmentedControl) {
     }
     
+    // MARK: - lifecycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createFonts()
+        
+        setupTextLayer()
+        
+        viewForTextLayer.layer.addSublayer(textLayer)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        textLayer.frame = viewForTextLayer.bounds
+        textLayer.fontSize = fontSize
+    }
+    
+    // MARK: - Helpr
+    
+    func setupTextLayer(){
+        
+        textLayer.frame = viewForTextLayer.bounds
+        
+        var string = ""
+        for _ in 1...20 {
+            string += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor arcu quis velit congue dictum. "
+        }
+        
+        textLayer.string = string
+        textLayer.font = helveticaFont
+        textLayer.foregroundColor = UIColor.darkGrayColor().CGColor
+        textLayer.wrapped = true
+        textLayer.alignmentMode = kCAAlignmentLeft
+        textLayer.truncationMode = kCATruncationEnd
+        textLayer.contentsScale = UIScreen.mainScreen().scale
+    }
+    
+    func createFonts() {
+        var fontName : CFStringRef = "Noteworthy-Light"
+        noteworthyLightFont = CTFontCreateWithName(fontName, baseFontSize, nil)
+        fontName = "Helvetica"
+        helveticaFont = CTFontCreateWithName(fontName, baseFontSize, nil)
+    }
 }
