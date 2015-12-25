59071c540fe97eef67399921be7de8e1c9c3bc64
Wizard Lee <282539259@qq.com>
2015-12-25 10:18:28 +0800

    minor change

---
+++ CALayerDemo/CAScrollLayer.swift
@@ -0,0 +1,13 @@
+//
+//  CAScrollLayer.swift
+//  CALayerDemo
+//
+//  Created by Wizard Li on 12/25/15.
+//  Copyright © 2015 Alchemist. All rights reserved.
+//
+
+import UIKit
+
+class CAScrollLayer: UIViewController {
+    
+}
---
+++ CALayerDemo/ScrollingView.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollingView.swift
+//  CALayerDemo
+//
+//  Created by Wizard Li on 12/25/15.
+//  Copyright © 2015 Alchemist. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ScrollingView : UIView {
+    override class func layerClass() -> AnyClass {
+        return CAScrollLayer.self
+    }
+}
