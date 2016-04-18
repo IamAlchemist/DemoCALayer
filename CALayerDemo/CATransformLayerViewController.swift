11354376273f067b49e3d0a421bcb74ecfac5d3b
IamAlchemist <wizard.lee@qq.com>
2016-04-18 15:30:47 +0800

    implement transform layer

--- CALayerDemo/CATransformLayerViewController.swift
+++ CALayerDemo/CATransformLayerViewController.swift
@@ -13,6 +13,18 @@ class CATransformLayerViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var perspective = CATransform3DIdentity
+        perspective.m34 = -1.0/500.0
+        view.layer.sublayerTransform = perspective
+        
+        let transform1 = CATransform3DMakeTranslation(-100, 0, 0)
+        view.layer.addSublayer(cubeWithTransform(transform1))
+        
+        var transform2 = CATransform3DIdentity
+        transform2 = CATransform3DTranslate(transform2, 100, 0, 0)
+        transform2 = CATransform3DRotate(transform2, CGFloat(-M_PI_4), 1, 0, 0)
+        transform2 = CATransform3DRotate(transform2, CGFloat(-M_PI_4), 0, 1, 0)
+        view.layer.addSublayer(cubeWithTransform(transform2))
     }
     
     func faceWithTransform(transform : CATransform3D) -> CALayer {
@@ -40,6 +52,25 @@ class CATransformLayerViewController : UIViewController {
         cube.addSublayer(faceWithTransform(ct))
         
         ct = CATransform3DMakeTranslation(0, -50, 0)
+        ct = CATransform3DRotate(ct, CGFloat(M_PI_2), 1, 0, 0)
+        cube.addSublayer(faceWithTransform(ct))
         
+        ct = CATransform3DMakeScale(0, 50, 0)
+        ct = CATransform3DRotate(ct, CGFloat(-M_PI_2), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        ct = CATransform3DMakeTranslation(-50, 0, 0)
+        ct = CATransform3DRotate(ct, CGFloat(-M_PI_2), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        ct = CATransform3DMakeTranslation(0, 0, -50)
+        ct = CATransform3DRotate(ct, CGFloat(M_PI), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        let contentSize = view.bounds.size
+        cube.position = CGPoint(x: contentSize.width / 2, y: contentSize.height / 2)
+        
+        cube.transform = transform
+        return cube
     }
 }
